e4b53af92ed4e71542793ef0c1e7a8055ceb1791
Keerthan <ans.keerthan135@gmail.com>
2022-02-16 17:04:34 +0530

    Using logging.basicConfig() instead of logging config file and set logging level to info

--- rule_generator.py
+++ rule_generator.py
@@ -10,35 +10,35 @@ import datetime
 
 def ingest_config():
     json_df = spark.read.option("multiline", "true").json("data/rules.json")
-    logging.debug("reading test json from file")
-    # logging.debug(json_df.printSchema())
+    logging.info("reading test json from file")
+    # logging.info(json_df.printSchema())
     return json_df
 
 
 def ingest_atm_file():
     # Reading the source atm file and loading into a dataframe
     atm_df = spark.read.option("Header", "true").option("InferSchema", "true").csv("data/atm.csv")
-    logging.debug("Reading atm transactions csv file")
+    logging.info("Reading atm transactions csv file")
     return atm_df
 
 
 def parse_json(json_df):
-    logging.debug("Parsing Json")
+    logging.info("Parsing Json")
 
     rulesExplodedDf = json_df.select(explode("rules").alias("rules")).select("rules.*")
-    # logging.debug(rulesExplodedDf.printSchema())
-    # logging.debug(rulesExplodedDf.show(truncate=False))
+    # logging.info(rulesExplodedDf.printSchema())
+    # logging.info(rulesExplodedDf.show(truncate=False))
 
     parentDf = rulesExplodedDf.select("id", "name", "description", "is_valid", "valid_from", "valid_till",
                                       explode("then").alias("then")) \
         .select("id", "name", "description", "is_valid", "valid_from", "valid_till", "then.*")
-    # logging.debug(parentDf.printSchema())
-    # logging.debug(parentDf.show(truncate=False))
+    # logging.info(parentDf.printSchema())
+    # logging.info(parentDf.show(truncate=False))
 
     childDf = rulesExplodedDf.select("id", explode("when").alias("when")) \
         .select("id", "when.*")
-    # logging.debug(childDf.printSchema())
-    # logging.debug(childDf.show(truncate=False))
+    # logging.info(childDf.printSchema())
+    # logging.info(childDf.show(truncate=False))
 
     return parentDf, childDf
 
@@ -53,11 +53,11 @@ def rules_pipeline(pdf, cdf, table_name):
 
     for i in pdf:
 
-        logging.debug("Looping through the json list")
+        logging.info("Looping through the json list")
 
         if check_rule:
             where_query = where_query + " and"
-            logging.debug("where_query > {}".format(where_query))
+            logging.info("where_query > {}".format(where_query))
 
         p_id = i["id"]
         p_name = i["name"]
@@ -71,7 +71,7 @@ def rules_pipeline(pdf, cdf, table_name):
         if p_is_valid == "true":
             if p_valid_till != 1:  # This needs to be replaced with if current date is in between valid from and
                 # valid till
-                logging.debug("Rule {} is valid and is being checked".format(p_id))
+                logging.info("Rule {} is valid and is being checked".format(p_id))
 
                 for j in cdf:
                     # print(row["field_name"] + ' > ' + row["field_value"] + ' > ' + row["join"] + ' > ' + row[
@@ -84,10 +84,10 @@ def rules_pipeline(pdf, cdf, table_name):
 
                     if check_rule:
                         validate_rule_id = check_rule_id
-                        logging.debug("check_rule is true and validate_rule_id is {}".format(validate_rule_id))
+                        logging.info("check_rule is true and validate_rule_id is {}".format(validate_rule_id))
                     else:
                         validate_rule_id = int(p_id)
-                        logging.debug(
+                        logging.info(
                             "check_rule is false therefore follow the queue and validate_rule_id is {}".format(
                                 validate_rule_id))
 
@@ -95,7 +95,7 @@ def rules_pipeline(pdf, cdf, table_name):
 
                         if not (j["join"] and j["join"].strip()) != "":
 
-                            logging.debug("Join is empty")
+                            logging.info("Join is empty")
 
                             if c_value.isnumeric():
                                 c_value = int(c_value)
@@ -104,18 +104,18 @@ def rules_pipeline(pdf, cdf, table_name):
                                     where_query = where_query + " {} {} {}%".format(c_name, c_operator, c_value)
                                 else:
                                     where_query = where_query + " {} {} {}".format(c_name, c_operator, c_value)
-                                logging.debug("query > {}".format(where_query))
+                                logging.info("query > {}".format(where_query))
                             else:
                                 '''df.filter(col("state") == = "OH")'''
                                 if c_operator == "LIKE" or c_operator == "NOT LIKE":
                                     where_query = where_query + " {} {} '{}%'".format(c_name, c_operator, c_value)
                                 else:
                                     where_query = where_query + " {} {} '{}'".format(c_name, c_operator, c_value)
-                                logging.debug("query > {}".format(where_query))
+                                logging.info("query > {}".format(where_query))
 
                         else:
 
-                            logging.debug("Join is not empty")
+                            logging.info("Join is not empty")
 
                             if c_value.isnumeric():
                                 c_value = int(c_value)
@@ -125,7 +125,7 @@ def rules_pipeline(pdf, cdf, table_name):
                                 else:
                                     where_query = where_query + " {} {} {}  {}".format(c_name, c_operator, c_value,
                                                                                        c_join)
-                                logging.debug("query > {}".format(where_query))
+                                logging.info("query > {}".format(where_query))
                             else:
                                 if c_operator == "LIKE" or c_operator == "NOT LIKE":
                                     where_query = where_query + " {} {} '{}%'  {}".format(c_name, c_operator, c_value,
@@ -134,45 +134,45 @@ def rules_pipeline(pdf, cdf, table_name):
                                     where_query = where_query + " {} {} '{}'  {}".format(c_name, c_operator,
                                                                                          c_value,
                                                                                          c_join)
-                                logging.debug("query > {}".format(where_query))
+                                logging.info("query > {}".format(where_query))
 
                 ttr_check = "true"
-                logging.debug("where query > {}".format(where_query))
+                logging.info("where query > {}".format(where_query))
                 rule_success = True
 
             else:
-                logging.debug("Rule {} and {} are out of range and is skipped".format(p_valid_from, p_valid_till))
+                logging.info("Rule {} and {} are out of range and is skipped".format(p_valid_from, p_valid_till))
                 rule_success = False
 
         else:
-            logging.debug("Rule {} is not valid and is skipped".format(p_id))
+            logging.info("Rule {} is not valid and is skipped".format(p_id))
             rule_success = False
 
         if rule_success:
-            logging.debug("Rule {} is success and the field name is {} ".format(p_id, p_field_name))
+            logging.info("Rule {} is success and the field name is {} ".format(p_id, p_field_name))
             if p_field_name == "lookup":
                 check_rule = True
                 check_rule_id = int(p_field_value)
-                logging.debug("check_rule_id is {} and check_rule is {} ".format(check_rule_id, check_rule))
+                logging.info("check_rule_id is {} and check_rule is {} ".format(check_rule_id, check_rule))
             else:
                 where_query = where_query + " order by id "
                 total_query = select_query + where_query
-                logging.debug("total_query > {}".format(total_query))
+                logging.info("total_query > {}".format(total_query))
 
     return total_query
 
 
 def rule_generator():
-    logging.debug("Rule generator has been called")
+    logging.info("Rule generator has been called")
 
     json_df = ingest_config()
-    # logging.debug(json_df.show(truncate=False))
+    # logging.info(json_df.show(truncate=False))
 
     # collecting the dataframe back to the driver to pass it as a list for forming the query
     pdf, cdf = parse_json(json_df)
     # Show the schema of parent and child dataframe on console
-    # logging.debug(pdf.printSchema())
-    # logging.debug(cdf.printSchema())
+    # logging.info(pdf.printSchema())
+    # logging.info(cdf.printSchema())
 
     pdf_collect = pdf.collect()
     cdf_collect = cdf.collect()
@@ -188,7 +188,7 @@ def rule_generator():
 
 
 if __name__ == '__main__':
-    logging.config.fileConfig("resources/logging.conf")
+    logging.basicConfig(level="INFO")
 
     spark = SparkSession \
         .builder \
@@ -197,9 +197,9 @@ if __name__ == '__main__':
         .enableHiveSupport() \
         .getOrCreate()
 
-    logging.debug("spark session created")
+    logging.info("spark session created")
 
-    logging.debug("Call the rule generator")
+    logging.info("Call the rule generator")
     total_query = rule_generator()
 
     with open("output/queries.txt", "w") as f:
@@ -207,7 +207,7 @@ if __name__ == '__main__':
             f.write("\n\n")
 
     tempDf = spark.sql(total_query)
-    tempDf.show(truncate=False)
-    tempDf.repartition(1).write.option("header", "true").csv("output/Dataframe")
+    logging.info(tempDf.show(truncate=False))
+    # tempDf.repartition(1).write.option("header", "true").csv("output/Dataframe")
 
 
